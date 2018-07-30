0d4094bcdece1977d86d2665f9ddf9597dcc9113
maddymthompson <madeline.thompson.17@tfacorps.org>
2018-07-30 02:55:29 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,3 +2,109 @@ class Owner
   # code goes here
 
  end
+
+class Cat
+  # code goes here
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
+
+  def name
+    @name
+  end
+
+  def mood=(mood)
+    @mood = mood
+  end
+
+  def mood
+    @mood
+  end
+end
+
+
+class Dog
+  # code goes here
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
+
+  def name
+    @name
+  end
+
+  def mood=(mood)
+    @mood = mood
+  end
+
+  def mood
+    @mood
+  end
+end
+
+class Fish
+  # code goes here
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
+
+  def mood=(mood)
+    @mood = mood
+  end
+
+  def mood
+    @mood
+  end
+
+  def name
+    @name
+  end
+end
+
+class Owner
+  # code goes here
+  def initialize(species)
+    @species = species
+    @pets = {:fishes => [], :dogs => [], :cats => []}
+  end
+
+  def pets
+    @pets
+  end
+
+  def buy_fish(fish)
+    @pets[:fishes] << fish
+  end
+
+  def buy_cat(cat)
+    @pets[:cats] << cat
+  end
+
+  def buy_dog(dog)
+    @pets[:dogs] << dog
+  end
+  def species
+    @species
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def list_pets
+    "I have #{@pets[:fishes].count} fish, #{@pets[:dogs].count} dog(s), and #{@pets[:cats].count} cat(s)."
+  end
+
+
+end
